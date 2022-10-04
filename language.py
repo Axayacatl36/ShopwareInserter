314fbdabe27fa277bb2dfb48ef4a84dfcc93faba
Axayacatl36 <sebastian.schneider9@mailbox.tu-dresden.de>
2022-10-04 14:43:36 +0200

    added continuos mode and table spinbox

--- language.py
+++ language.py
@@ -24,21 +24,29 @@ class Language():
             uploadAction    = "Upload"
             helpAction = "Help"
             changeLanguageAction = "Change Language"
+            continuosModeAction   = "Activate continuos renameing"
+            continuosModeActionOff   = "Deactivate continuos renameing"
         
         class Hints():
             openHint    = "Open a excel, pdf or csv file"
             newHint     = "Add new row to current view" 
             deleteHint  = "Delete a row from current view"
             removeTableHint = "Remove table from list"
-            saveHint    = "Save all products of current table"
-            saveAllHint = "Save all products of all current tables"
+            saveHint    = "Save products to importlist"
+            saveAllHint = "Save all products to importlist"
             backHint    = "Go back to previous table"
             nextHint    = "Go to next table"
             translateHint = "Translate selected Products to selected Language"
-            uploadHint  = "Uploads all saved and flagged products to Shopware"
+            uploadHint  = "Uploads marked products to shopware"
             helpHint    = "Help Content"
             changeLanguageHint = "Choose new language"
+            continuosModeHint = "Rename column title in following tables"
+            continuosModeHintOff = "Rename column title in active table"
             currentTableHint = "Current table"
+        
+        class Label():
+            variantsLabel = "Variants"
+            categoryLabel = "Category"
         
         class Menu():
             file    = "File"
@@ -46,9 +54,20 @@ class Language():
             help  = "Help"
         
         class Tabs():
-            data    = "Import Data"
-            products = "Product Overview"
+            data    = "Open Catalogue"
+            products = "Import List"
             detail  ="Details"
+        
+        class ImportSelection(enum.Enum):
+            Productname     = 0
+            Productnumber   = 1
+            PriceNetto      = 2
+            PriceBrutto     = 3
+            Amount          = 4
+            Stock           = 5
+            Description     = 6
+            MinimumPurchase = 7
+            Ignore          = 8
             
         
     class German():
@@ -70,23 +89,31 @@ class Language():
             nextAction      = "Nächste Tabelle"
             translateAction = "Übersetzen"
             uploadAction    = "Hochladen"
-            helpAction    = "Hilfe"
+            helpAction      = "Hilfe"
             changeLanguageAction = "Sprache ändern"
+            continuosModeAction   = "Kontinuierliche Umbenennung aktiviert"
+            continuosModeActionOff   = "Kontinuierliche Umbenennung deaktiviert"
         
         class Hints():
             openHint    = "Öffne eine Excel, PDF oder CSV Datei"
             newHint     = "Neue Reihe der aktuellen Ansicht hinzufügen"
             deleteHint  = "Reihe aus aktueller Ansicht entfernen"
             removeTableHint = "Entferne Tabelle aus der aktuellen liste"
-            saveHint    = "Speicher alle Produkte der aktuellen Tabelle"
-            saveAllHint = "Speicher alle Produkte von allen Tabellen"
+            saveHint    = "Tabelle in Importliste speichern"
+            saveAllHint = "Alle Tabellen in Importliste speichern"
             backHint    = "Gehe zurück zur vorherigen Tabelle"
             nextHint    = "Gehe zur nächsten Tabelle"
             translateHint = "Übersetze alle ausgewählten Produkte in die gewünschte Sprache"
-            uploadHint  = "Lädt alle gespeicherten und markierten Produkte zu Shopware hoch"
+            uploadHint  = "Markierte Artikel zu Shopware hochladen"
             helpHint    = "Hilfe Inhalt"
             changeLanguageHint = "Wähle die neue Sprache"
             currentTableHint = "Aktuelle Tabelle"
+            continuosModeHint = "Spaltentitel in nachfolgenden Tabellen umbenennen"
+            continuosModeHintOff = "Spaltentitel in aktiver Tabelle umbenennen"
+        
+        class Label():
+            variantsLabel = "Varianten"
+            categoryLabel = "Kategorien"
         
         class Menu():
             file    = "Datei"
@@ -94,9 +121,20 @@ class Language():
             help    = "Hilfe"
         
         class Tabs():
-            data    = "Daten importieren"
-            products = "Übersicht"
+            data    = "Katalog öffnen"
+            products = "Importliste"
             detail  ="Details"
+        
+        class ImportSelection(enum.Enum):
+            Produktname     = 0
+            Produktnummer   = 1
+            PreisNetto      = 2
+            PreisBrutto     = 3
+            Anzahl          = 4
+            Lagerbestand    = 5
+            Beschreibung    = 6
+            Mindestabnahme  = 7
+            Ignorieren      = 8
 
 class TranslationLanguages(enum.Enum):
     English = "en"
